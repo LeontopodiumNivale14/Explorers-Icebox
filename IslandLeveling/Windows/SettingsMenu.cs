5e0293d1721fbf02c02302d72ca6e7ed90fb1c27
Leontopodium Nivale <103514618+LeontopodiumNivale14@users.noreply.github.com>
2024-11-26 12:02:00 -0500

    IT'S WORKING. IT'S ACTUALLY WORKING.
    -> *-actually-* got the GetItemCount() to dynamically update the values of the items. Now it shows it in the settings menu
    -> Made the settings menu cleaner. Still needs some work but it's a step in the right direction

--- IslandLeveling/Windows/SettingsMenu.cs
+++ IslandLeveling/Windows/SettingsMenu.cs
@@ -64,10 +64,12 @@ namespace IslandLeveling.Windows
 
             ImGui.PushItemWidth(100);
             ImGui.PopItemWidth();
-            ImGui.TextWrapped("Island Sanctuary Items to keep: \nInput how many of them you need for workshop");
+            ImGui.TextWrapped("Island Sanctuary Items to keep. Please input how many of them you need for workshop");
             ImGui.Separator();
             
-            ImGui.Text($"Palm Leaf | Current Amount: {PalmLeafAmount}");
+            ImGui.Text($"Palm Leaf (Have: {PalmLeafAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Palm Leaf", ref palmleafSend))
             {
                 if (palmleafSend < 0) palmleafSend = 0;
@@ -75,7 +77,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
             
-            ImGui.Text("Branch");
+            ImGui.Text($"Branch (Have: {BranchAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Branch Send", ref branchSend))
             {
                 if (branchSend < 0) branchSend = 0;
@@ -83,7 +87,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
             
-            ImGui.Text("Stone");
+            ImGui.Text($"Stone (Have: {StoneAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Stone Send", ref stoneSend))
             {
                 if (stoneSend < 0) stoneSend = 0;
@@ -91,7 +97,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Clam");
+            ImGui.Text($"Clam (Have: {ClamAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Clam Send", ref clamSend))
             {
                 if (clamSend < 0) clamSend = 0;
@@ -99,7 +107,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Laver");
+            ImGui.Text($"Laver (Have: {LaverAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Laver Send", ref laverSend))
             {
                 if (laverSend < 0) laverSend = 0;
@@ -107,7 +117,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
             
-            ImGui.Text("Coral");
+            ImGui.Text($"Coral (Have: {CoralAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Coral Send", ref coralSend))
             {
                 if (coralSend < 0) coralSend = 0;
@@ -115,7 +127,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Islewort");
+            ImGui.Text($"Islewort (Have: {IslewortAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Islewort Send", ref islewortSend))
             {
                 if (islewortSend < 0) islewortSend = 0;
@@ -123,7 +137,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Sand");
+            ImGui.Text($"Sand (Have: {SandAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Sand Send", ref sandSend))
             {
                 if (sandSend < 0) sandSend = 0;
@@ -131,7 +147,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Vine");
+            ImGui.Text($"Vine (Have: {VineAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Vine Send", ref vineSend))
             {
                 if (vineSend < 0) vineSend = 0;
@@ -139,7 +157,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Sap");
+            ImGui.Text($"Sap (Have: {SapAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Sap Send", ref sapSend))
             {
                 if (sapSend < 0) sapSend = 0;
@@ -147,7 +167,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Apple");
+            ImGui.Text($"Apple (Have: {AppleAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Apple Send", ref appleSend))
             {
                 if (appleSend < 0) appleSend = 0;
@@ -155,7 +177,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Log");
+            ImGui.Text($"Log (Have: {LogAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Log Send", ref logSend))
             {
                 if (logSend < 0) logSend = 0;
@@ -163,7 +187,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
             
-            ImGui.Text("Palm Log");
+            ImGui.Text($"Palm Log (Have: {PalmLogAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Palm Log Send", ref palmlogSend))
             {
                 if (palmlogSend < 0) palmlogSend = 0;
@@ -171,7 +197,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Copper");
+            ImGui.Text($"Copper (Have: {CopperAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Copper Send", ref copperSend))
             {
                 if (copperSend < 0) copperSend = 0;
@@ -179,7 +207,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Limestone");
+            ImGui.Text($"Limestone (Have: {LimestoneAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Limestone Send", ref limestoneSend))
             {
                 if (limestoneSend < 0) limestoneSend = 0;
@@ -187,7 +217,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Rock Salt");
+            ImGui.Text($"Rock Salt (Have: {RockSaltAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Rock Salt Send", ref rocksaltSend))
             {
                 if (rocksaltSend < 0) rocksaltSend = 0;
@@ -195,7 +227,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Clay");
+            ImGui.Text($"Clay (Have: {ClayAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Clay Send", ref claySend))
             {
                 if (claySend < 0) claySend = 0;
@@ -203,7 +237,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Tinsand");
+            ImGui.Text($"Tinsand (Have: {TinsandAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Tinsand Send", ref tinsandSend))
             {
                 if (tinsandSend < 0) tinsandSend = 0;
@@ -211,7 +247,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Sugarcane");
+            ImGui.Text($"Sugarcane (Have: {SugarcaneAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Sugarcane Send", ref sugarcaneSend))
             {
                 if (sugarcaneSend < 0) sugarcaneSend = 0;
@@ -219,7 +257,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Cotton");
+            ImGui.Text($"Cotton (Have: {CottonAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Cotton Send", ref cottonSend))
             {
                 if (cottonSend < 0) cottonSend = 0;
@@ -227,7 +267,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Hemp");
+            ImGui.Text($"Hemp (Have: {HempAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Hemp Send", ref hempSend))
             {
                 if (hempSend < 0) hempSend = 0;
@@ -235,7 +277,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Islefish");
+            ImGui.Text($"Islefish (Have: {IslefishAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Islefish Send", ref islefishSend))
             {
                 if (islefishSend < 0) islefishSend = 0;
@@ -243,7 +287,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Squid");
+            ImGui.Text($"Squid (Have: {SquidAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Squid Send", ref squidSend))
             {
                 if (squidSend < 0) squidSend = 0;
@@ -251,7 +297,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Jellyfish");
+            ImGui.Text($"Jellyfish (Have: {JellyfishAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Jellyfish Send", ref jellyfishSend))
             {
                 if (jellyfishSend < 0) jellyfishSend = 0;
@@ -259,7 +307,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Iron Ore");
+            ImGui.Text($"Iron Ore (Have: {IronOreAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Iron Ore Send", ref ironoreSend))
             {
                 if (ironoreSend < 0) ironoreSend = 0;
@@ -267,7 +317,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Quartz");
+            ImGui.Text($"Quartz (Have: {QuartzAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Quartz Send", ref quartzSend))
             {
                 if (quartzSend < 0) quartzSend = 0;
@@ -275,7 +327,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Leucogranite");
+            ImGui.Text($"Leucogranite (Have: {LeucograniteAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Leucogranite Send", ref leucograniteSend))
             {
                 if (leucograniteSend < 0) leucograniteSend = 0;
@@ -283,7 +337,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Multicolored Isleblooms");
+            ImGui.Text($"Multicolored Isleblooms (Have: {MulticoloredIslebloomsAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Multicolored Isleblooms Send", ref multicoloredislebloomsSend))
             {
                 if (multicoloredislebloomsSend < 0) multicoloredislebloomsSend = 0;
@@ -291,7 +347,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Resin");
+            ImGui.Text($"Resin (Have: {ResinAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Resin Send", ref resinSend))
             {
                 if (resinSend < 0) resinSend = 0;
@@ -299,7 +357,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Coconut");
+            ImGui.Text($"Coconut (Have: {CoconutAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Coconut Send", ref coconutSend))
             {
                 if (coconutSend < 0) coconutSend = 0;
@@ -307,7 +367,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Beehive");
+            ImGui.Text($"Beehive (Have: {BeehiveAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Beehive Send", ref beehiveSend))
             {
                 if (beehiveSend < 0) beehiveSend = 0;
@@ -315,7 +377,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Wood Opal");
+            ImGui.Text($"Wood Opal (Have: {WoodOpalAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Wood Opal Send", ref woodopalSend))
             {
                 if (woodopalSend < 0) woodopalSend = 0;
@@ -323,7 +387,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Coal");
+            ImGui.Text($"Coal (Have: {CoalAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Coal Send", ref coalSend))
             {
                 if (coalSend < 0) coalSend = 0;
@@ -331,7 +397,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Glimshroom");
+            ImGui.Text($"Glimshroom (Have: {GlimshroomAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Glimshroom Send", ref glimshroomSend))
             {
                 if (glimshroomSend < 0) glimshroomSend = 0;
@@ -339,7 +407,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Effervescent Water");
+            ImGui.Text($"Effervescent Water (Have: {EffervescentWaterAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Effervescent Water Send", ref effervescentwaterSend))
             {
                 if (effervescentwaterSend < 0) effervescentwaterSend = 0;
@@ -347,7 +417,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Shale");
+            ImGui.Text($"Shale (Have: {ShaleAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Shale Send", ref shaleSend))
             {
                 if (shaleSend < 0) shaleSend = 0;
@@ -355,7 +427,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Marble");
+            ImGui.Text($"Marble (Have: {MarbleAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Marble Send", ref marbleSend))
             {
                 if (marbleSend < 0) marbleSend = 0;
@@ -363,7 +437,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Mythril Ore");
+            ImGui.Text($"Mythril Ore (Have: {MythrilOreAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Mythril Ore Send", ref mythriloreSend))
             {
                 if (mythriloreSend < 0) mythriloreSend = 0;
@@ -371,7 +447,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Spectrine");
+            ImGui.Text($"Spectrine (Have: {SpectrineAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Spectrine Send", ref spectrineSend))
             {
                 if (spectrineSend < 0) spectrineSend = 0;
@@ -379,7 +457,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Durium Sand");
+            ImGui.Text($"Durium Sand (Have: {DuriumSandAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Durium Sand Send", ref duriumsandSend))
             {
                 if (duriumsandSend < 0) duriumsandSend = 0;
@@ -387,7 +467,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Yellow Copper Ore");
+            ImGui.Text($"Yellow Copper Ore (Have: {YellowCopperOreAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Yellow Copper Ore Send", ref yellowcopperoreSend))
             {
                 if (yellowcopperoreSend < 0) yellowcopperoreSend = 0;
@@ -395,7 +477,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Gold Ore");
+            ImGui.Text($"Gold Ore (Have: {GoldOreAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Gold Ore Send", ref goldoreSend))
             {
                 if (goldoreSend < 0) goldoreSend = 0;
@@ -403,7 +487,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Hawk's Eye Sand");
+            ImGui.Text($"Hawk's Eye Sand (Have: {HawksEyeSandAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Hawk's Eye Sand Send", ref hawkseyesandSend))
             {
                 if (hawkseyesandSend < 0) hawkseyesandSend = 0;
@@ -411,7 +497,9 @@ namespace IslandLeveling.Windows
             }
             ImGui.NewLine();
 
-            ImGui.Text("Crystal Formation");
+            ImGui.Text($"Crystal Formation (Have: {CrystalFormationAmount})");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100);
             if (ImGui.InputInt("##Crystal Formation Send", ref crystalformationSend))
             {
                 if (crystalformationSend < 0) crystalformationSend = 0;
